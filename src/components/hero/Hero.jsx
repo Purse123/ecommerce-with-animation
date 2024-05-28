4b7457e7ad3341e381f04fa716a1b71c759c3007
Purse123 <sahasneupane85@gmail.com>
2024-05-29 04:02:34 +0545

    navbar not selecting issue solved

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,9 +4,9 @@ import video from "/medium.mp4"
 
 const Hero = () => {
   return (
-    <div className='hero h-screen w-full '>
+    <div className='hero h-screen w-full'>
       <div className="h-[calc(100vh-150px)] w-full bg-white overflow-hidden">
-        <video autoPlay muted loop className='object-cover h-full w-full' src={video} >
+        <video autoPlay muted loop className='object-cover h-full w-full pointer-events-none z-50' src={video} >
           video not supported
         </video>
       </div>
